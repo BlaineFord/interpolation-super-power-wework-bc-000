f79c952984a88e9fc1fba3352d2d6b6034cc1abf
Blaine Ford <bford@wework.com>
2018-02-13 04:09:37 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,8 @@
 # Write your #display_rainbow method here
 display_rainbow = ["red","orange","yellow","green","blue","indigo","violet"]
 
-def #display_rainbow
-  
-  print "R:#{display_rainbow[0]}"
-
+def #display_rainbow 
+print "R:#{display_rainbow[0]}"
 end
 
 #display_rainbow
